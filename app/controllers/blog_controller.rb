5d3b7a00239f7b6aa0f92938cc4584314c61d90f
RoderickVongsa <roderick.vongsa@gmail.com>
2024-01-31 13:02:22 -0800

    blog new

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -7,4 +7,23 @@ class BlogController < ApplicationController
         @blog_post = Blog.find(params[:id])
     end 
 
+    def new
+        @blog_new = Blog.new
+    end 
+
+    def create
+        @blog_new = Blog.create(blog_params)
+        if @blog_new.valid?
+          flash[:success] = "Blog successfully created!"
+          redirect_to blogs_path
+        else
+            flash.now[:error] = "Failed to create blog."
+        end
+    end
+    private
+
+    def blog_params
+        params.require(:blog).permit(:title, :content)
+    end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
-  get '/blog', to: 'blog#index', as: 'blog'
-  get '/blog/:id', to: 'blog#show', as: 'blog_show'
+  get '/blogs', to: 'blog#index', as: 'blogs'
+  get '/blogs/new', to: 'blog#new', as: 'new'
+  post '/blogs', to: 'blog#create'
+  get '/blogs/:id', to: 'blog#show', as: 'blog'
+  
 end
 
