810b45d6a0a174efbfb1d8edfdded33699719321
RoderickVongsa <roderick.vongsa@gmail.com>
2024-01-31 10:11:50 -0800

    completed story 2

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -2,4 +2,9 @@ class BlogController < ApplicationController
     def index 
         @blog_list = Blog.all
     end 
+    
+    def show 
+        @blog_post = Blog.find(params[:id])
+    end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get '/blog', to: 'blog#index', as: 'blog'
+  get '/blog/:id', to: 'blog#show', as: 'blog_show'
 end
+
